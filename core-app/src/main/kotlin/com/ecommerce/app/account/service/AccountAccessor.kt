6e347cc2a1cdcc2759129bae9ddd216f6da26728
HJLee <gotjd8515@gmail.com>
2025-11-21 11:07:57 +0900

    refactor: 계정 조회 리드온리 추가

--- core-app/src/main/kotlin/com/ecommerce/app/account/service/AccountAccessor.kt
+++ core-app/src/main/kotlin/com/ecommerce/app/account/service/AccountAccessor.kt
@@ -3,12 +3,14 @@ package com.ecommerce.app.account.service
 import com.ecommerce.app.account.port.out.AccountRepository
 import com.ecommerce.app.account.service.exception.AccountNotFoundException
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class AccountAccessor(
     private val accountRepository: AccountRepository,
 ) {
 
+    @Transactional(readOnly = true)
     fun readByEmail(email: String) = accountRepository.findByEmail(email)
         ?: throw AccountNotFoundException()
 
