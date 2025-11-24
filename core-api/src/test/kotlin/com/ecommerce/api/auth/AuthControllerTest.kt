2e8d8f14d0d28050e1636b102ec748d5abb69d08
HJLee <gotjd8515@gmail.com>
2025-11-24 17:18:18 +0900

    feat: Auth restdocs 추가

--- core-api/src/test/kotlin/com/ecommerce/api/auth/AuthControllerTest.kt
+++ core-api/src/test/kotlin/com/ecommerce/api/auth/AuthControllerTest.kt
@@ -2,58 +2,90 @@ package com.ecommerce.api.auth
 
 import com.ecommerce.api.errors.ErrorType
 import com.ecommerce.api.response.ResultType
-import com.ecommerce.api.support.AbstractWebMvcTest
+import com.ecommerce.api.support.AbstractRestDocumentTest
 import com.ecommerce.app.account.service.exception.AccountNotFoundException
 import com.ecommerce.app.auth.port.`in`.LoginResult
 import com.ecommerce.app.auth.service.exception.PasswordMisMatchException
+import com.epages.restdocs.apispec.ResourceSnippetParameters
+import com.epages.restdocs.apispec.Schema
 import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.post
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders
+import org.springframework.restdocs.payload.JsonFieldType
+import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-class AuthControllerTest : AbstractWebMvcTest() {
-
+class AuthControllerTest : AbstractRestDocumentTest() {
 
     @Test
-    fun `계정이 존재하지 않으면 NotFound를 반환한다`() {
+    fun `로그인 실패 - 존재하지 않는 계정`() {
         val req = LoginRequest(email = "test@ecommerce.com", password = "testpassword")
 
         every { loginUseCase.login(any()) } throws AccountNotFoundException()
 
 
-        val resultActions = mockMvc.post("/api/v1/auth/login") {
-            contentType = MediaType.APPLICATION_JSON
-            content = gson.toJson(req)
-        }
+        val resultActions = mockMvc.perform(
+            RestDocumentationRequestBuilders
+                .post("/api/v1/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(req))
+        )
 
-        resultActions.andExpectAll {
-            status { isNotFound() }
-            jsonPath("$.result") { value(ResultType.ERROR.name) }
-            jsonPath("$.status") { value(404) }
-            jsonPath("$.data") { value(null) }
-            jsonPath("$.error.message") { value(ErrorType.Account.NOT_FOUND.message) }
-        }
+        resultActions.andExpectAll(
+            status().isNotFound,
+            jsonPath("$.result").value(ResultType.ERROR.name),
+            jsonPath("$.status").value(404),
+            jsonPath("$.data").value(null),
+            jsonPath("$.error.message").value(ErrorType.Account.NOT_FOUND.message)
+        )
+
+
+        resultActions.andDocument(
+            "로그인 - 실패 - 존재하지 않는 계정",
+            ResourceSnippetParameters.builder()
+                .tag("Auth")
+                .requestSchema(Schema("LoginRequest"))
+                .requestFields(*loginRequestFields().toTypedArray())
+                .responseSchema(Schema("ApiResponse<Unit>"))
+                .responseFields(*commonFailureResponseFields().toTypedArray())
+                .build()
+        )
+
     }
 
     @Test
-    fun `비밀번호가 일치하지 않으면 BadRequest를 반환한다`() {
+    fun `로그인 실패 - 비밀번호 불일치`() {
         val req = LoginRequest(email = "test@ecommerce.com", password = "testpassword")
 
         every { loginUseCase.login(any()) } throws PasswordMisMatchException()
 
 
-        val resultActions = mockMvc.post("/api/v1/auth/login") {
-            contentType = MediaType.APPLICATION_JSON
-            content = gson.toJson(req)
-        }
+        val resultActions = mockMvc.perform(
+            RestDocumentationRequestBuilders
+                .post("/api/v1/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(req))
+        )
+        resultActions.andExpectAll(
+            status().isBadRequest,
+            jsonPath("$.result").value(ResultType.ERROR.name),
+            jsonPath("$.status").value(400),
+            jsonPath("$.data").value(null),
+            jsonPath("$.error.message").value(ErrorType.Auth.PASSWORD_MISMATCH.message)
+        )
 
-        resultActions.andExpectAll {
-            status { isBadRequest() }
-            jsonPath("$.result") { value(ResultType.ERROR.name) }
-            jsonPath("$.status") { value(400) }
-            jsonPath("$.data") { value(null) }
-            jsonPath("$.error.message") { value(ErrorType.Auth.PASSWORD_MISMATCH.message) }
-        }
+        resultActions.andDocument(
+            "로그인 - 실패 - 비밀번호 불일치",
+            ResourceSnippetParameters.builder()
+                .tag("Auth")
+                .requestSchema(Schema("LoginRequest"))
+                .requestFields(*loginRequestFields().toTypedArray())
+                .responseSchema(Schema("ApiResponse<Unit>"))
+                .responseFields(*commonFailureResponseFields().toTypedArray())
+                .build()
+        )
 
     }
 
@@ -68,22 +100,47 @@ class AuthControllerTest : AbstractWebMvcTest() {
         )
         every { loginUseCase.login(any()) } returns result
 
+        val resultActions = mockMvc.perform(
+            RestDocumentationRequestBuilders
+                .post("/api/v1/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(req))
+        )
 
-        val resultActions = mockMvc.post("/api/v1/auth/login") {
-            contentType = MediaType.APPLICATION_JSON
-            content = gson.toJson(req)
-        }
+        resultActions.andExpectAll(
+            status().isOk,
+            jsonPath("$.result").value(ResultType.SUCCESS.name),
+            jsonPath("$.status").value(200),
+            jsonPath("$.data.email").value(result.email),
+            jsonPath("$.data.name").value(result.name),
+            jsonPath("$.data.accessToken").value(result.accessToken),
+            jsonPath("$.error").value(null)
+        )
 
-        resultActions.andExpectAll {
-            status { isOk() }
-            jsonPath("$.result") { value(ResultType.SUCCESS.name) }
-            jsonPath("$.status") { value(200) }
-            jsonPath("$.data.email") { value(result.email) }
-            jsonPath("$.data.name") { value(result.name) }
-            jsonPath("$.data.accessToken") { value(result.accessToken) }
-            jsonPath("$.error") { value(null) }
-        }
-
+        resultActions.andDocument(
+            "로그인 - 성공",
+            ResourceSnippetParameters.builder()
+                .tag("Auth")
+                .summary("로그인")
+                .description("로그인 API")
+                .requestSchema(Schema("LoginRequest"))
+                .requestFields(*loginRequestFields().toTypedArray())
+                .responseSchema(Schema("ApiResponse<LoginResult>"))
+                .responseFields(*loginSuccessResponseFields().toTypedArray())
+                .build()
+        )
     }
+
+    private fun loginRequestFields() = listOf(
+        fieldWithPath("email").type(JsonFieldType.STRING).description("계정 이메일"),
+        fieldWithPath("password").type(JsonFieldType.STRING).description("계정 비밀번호"),
+    )
+
+    private fun loginSuccessResponseFields() = commonSuccessResponseFields(
+        fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("계정 고유 아이디"),
+        fieldWithPath("data.email").type(JsonFieldType.STRING).description("계정 이메일"),
+        fieldWithPath("data.name").type(JsonFieldType.STRING).description("계정 이름"),
+        fieldWithPath("data.accessToken").type(JsonFieldType.STRING).description("엑세스 토큰")
+    )
 
 }
