36817dba9c7798e48d822ffce0266a2e12728dc2
HJLee <gotjd8515@gmail.com>
2025-11-24 15:08:24 +0900

    chore: settings openapi3

--- core-api/build.gradle.kts
+++ core-api/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.epages.restdocs-api-spec") version "0.18.2"
+    id("com.epages.restdocs-api-spec") version "0.19.4"
 }
 dependencies {
     runtimeOnly(project(":core-infra:jpa"))
@@ -14,6 +14,23 @@ dependencies {
     implementation("com.google.code.gson:gson:2.13.0")
 
     testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("com.epages:restdocs-api-spec-mockmvc:0.18.2")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("com.epages:restdocs-api-spec-mockmvc:0.19.4")
 
+}
+
+openapi3 {
+    this.setServer("http://localhost:8082/")
+    title = "E-Commerce API"
+    description = "E-Commerce API Documentation"
+    version = "1.0.0"
+    format = "yaml"
+    outputDirectory = "src/main/resources/static/docs"
+}
+
+tasks.register<Copy>("copyOasToSwagger") {
+    delete("src/main/resources/static/docs/openapi3.yaml") // 기존 OAS 파일 삭제
+    from(layout.buildDirectory.file("api-spec/openapi3.yaml")) // 복제할 OAS 파일 지정
+    into("src/main/resources/static/docs/.") // 타겟 디렉터리로 파일 복제
+    dependsOn("openapi3") // openapi3 Task가 먼저 실행되도록 설정
 }
