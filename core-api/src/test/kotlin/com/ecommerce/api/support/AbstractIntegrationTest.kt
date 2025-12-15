600084fe77728f545c1cba67c18318a72582f2aa
HJLee <gotjd8515@gmail.com>
2025-12-15 12:12:52 +0900

    test: 통합 테스트 환경 설정

--- core-api/src/test/kotlin/com/ecommerce/api/support/AbstractIntegrationTest.kt
+++ core-api/src/test/kotlin/com/ecommerce/api/support/AbstractIntegrationTest.kt
@@ -3,9 +3,11 @@ package com.ecommerce.api.support
 import com.ecommerce.infra.jpa.DatabaseCleanupExtension
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestConstructor
 
 @SpringBootTest
+@ActiveProfiles("test")
 @ExtendWith(DatabaseCleanupExtension::class)
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 abstract class AbstractIntegrationTest
